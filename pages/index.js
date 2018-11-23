96f08971754bfc170020575f20c12da3d5d2658d
Manot Luijiu <manot_luijiu@hotmail.com>
2018-11-23 16:31:25 +0700

    adding express

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 import '../scss/main.scss';
 
 const Index = () => (
   <section style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Build up digital world</title>
+    </Head>
     <Navbar />
     <h1>Hello world from Next JS</h1>
   </section>
