527d1295da9e7afe2b4da70a9a6f2c8cd8861b32
Manot Luijiu <manot_luijiu@hotmail.com>
2018-11-23 15:11:31 +0700

    adding assets

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,35 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  state = {};
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
-          <title>Build up digital world</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="google" content="notranslate" />
+          <meta name="theme-color" content="#1976D2" />
+
+          <link
+            rel="shortcut icon"
+            href="https://assets.means-business.com/favicon-32x32.png"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Exo+2|Roboto|Rubik"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+          <link
+            rel="stylesheet"
+            href="https://assets.means-business.com/nprogress.min.css"
+          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar';
+import '../scss/main.scss';
 
 const Index = () => (
   <section>
