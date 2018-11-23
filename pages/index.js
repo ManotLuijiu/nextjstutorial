d32bff1050e86462ede284893678d3fcc1b14e30
Manot Luijiu <manot_luijiu@hotmail.com>
2018-11-23 15:54:44 +0700

    adding material-ui prop-types

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar';
 import '../scss/main.scss';
 
 const Index = () => (
-  <section>
+  <section style={{ padding: '10px 45px' }}>
     <Navbar />
     <h1>Hello world from Next JS</h1>
   </section>
